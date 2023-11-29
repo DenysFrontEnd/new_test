3650a4e3198e68ef10366f42bc24fff0c8181674
DenysFrontEnd <112694680+DenysFrontEnd@users.noreply.github.com>
2023-11-29 13:58:58 +0000

    Deploying to gh-pages from @ DenysFrontEnd/new_test@d191712ec20ce13a1d3831b352d68181ee672793 🚀

---
+++ index.d1a396cf.js
@@ -0,0 +1,2 @@
+console.log("hello!"),new Swiper(".swiper",{loop:!0,slidesPerView:"auto",breakpoints:{375:{slidesPerView:1},768:{slidesPerView:3,spaceBetween:2},1e3:{spaceBetween:3,slidesPerView:3}},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},scrollbar:{el:".swiper-scrollbar"}});
+//# sourceMappingURL=index.d1a396cf.js.map
